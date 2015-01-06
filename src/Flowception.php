2314d959de05c0e81f8aa08eb5961d98509808fb
Oliver Hader <oliver@typo3.org>
2015-01-06 18:50:37 +0100

    [TASK] Kick-off extension

---
+++ src/Flowception.php
@@ -0,0 +1,13 @@
+<?php
+namespace Codeception\Extension;
+
+use Codeception\Exception\Extension as ExtensionException;
+
+class Flowception extends \Codeception\Platform\Extension {
+
+	public function __construct($config, $options) {
+
+	}
+
+}
+
